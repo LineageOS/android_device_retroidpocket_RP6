47b15deef7cacda9644f282208ee3a3314c55a29
AnierinB <anierin@evolution-x.org>
2026-01-20 04:17:27 -0800

    RP6: Initial skeleton tree
    Change-Id: Ic1331056aa058487059f0880bf1f2edb9ad10bfe
    Signed-off-by: AnierinB <anierin@evolution-x.org>

---
+++ device.mk
@@ -0,0 +1,14 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
+# Inherit from the common OEM chipset makefile.
+$(call inherit-product, device/ayn/qcs8550-common/common.mk)
+
+# Inherit from the proprietary files makefile.
+$(call inherit-product, vendor/retroidpocket/RP6/RP6-vendor.mk)
---
+++ extract-files.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    'vendor/ayn/qcs8550-common',
+]
+
+module = ExtractUtilsModule(
+    'RP6',
+    'retroidpocket',
+    namespace_imports=namespace_imports,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device_with_common(
+        module, 'qcs8550-common', 'ayn'
+    )
+    utils.run()
