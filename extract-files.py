2ae414bf6348a9ec07632c84db394a43f4a24990
AnierinB <anierin@evolution-x.org>
2026-02-04 17:32:17 +0000

    RP6: Setup default display orientation
    Change-Id: I065db94a7dcb50ae575ac4e9524533874b3784f0
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- extract-files.py
+++ extract-files.py
@@ -4,6 +4,11 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+
 from extract_utils.main import (
     ExtractUtils,
     ExtractUtilsModule,
@@ -13,9 +18,17 @@ namespace_imports = [
     'vendor/ayn/qcs8550-common',
 ]
 
+blob_fixups: blob_fixups_user_type = {
+    'vendor/etc/sensors/config/kailua_qmi8658_0.json': blob_fixup()
+        .regex_replace('"ver": "0"', '"ver": "1"')
+        .regex_replace('"data": "-y"', '"data": "+x"')
+        .regex_replace('"data": "-x"', '"data": "-y"'),
+}  # fmt: skip
+
 module = ExtractUtilsModule(
     'RP6',
     'retroidpocket',
+    blob_fixups=blob_fixups,
     namespace_imports=namespace_imports,
     add_firmware_proprietary_file=True,
 )
