bffa10bcd65846d0ee5066d584ffd3cfe5de9b1a
AnierinB <anierin@evolution-x.org>
2026-03-31 04:51:03 +0000

    RP6: Filter dtbos
    Change-Id: I47fad191f7a7aaa17b9074de9d27c6bade09cc23
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -8,6 +8,9 @@ DEVICE_PATH := device/retroidpocket/RP6
 # Include the common OEM chipset BoardConfig.
 include device/ayn/qcs8550-common/BoardConfigCommon.mk
 
+# DTBO
+TARGET_MERGE_DTBOS_WILDCARD := *retroid-pocket-6*
+
 # Properties
 DEVICE_PROPERTIES_PATH := $(DEVICE_PATH)/properties
 TARGET_VENDOR_PROP += $(DEVICE_PROPERTIES_PATH)/vendor.prop
