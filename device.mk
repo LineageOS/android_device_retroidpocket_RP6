01019e62668696c938407f7f4626c0452b3b96ce
AnierinB <anierin@evolution-x.org>
2026-04-08 00:22:01 +0000

    RP6: Include mcuapp_firmware.txt
    Change-Id: I5e981e700fe39ef6ba1e12c46160adbf83b0b52d
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- device.mk
+++ device.mk
@@ -7,6 +7,10 @@
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/idc/fts_ts.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/fts_ts.idc
 
+# MCU
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/mcu/mcuapp_firmware.txt:$(TARGET_COPY_OUT_VENDOR)/firmware/mcuapp_firmware.txt
+
 # Overlay
 PRODUCT_PACKAGES += \
     Frameworks-RP6-Overlay \
